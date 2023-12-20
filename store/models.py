ecda0d34869d077b3944643ed96181c8fa3f399c
pupetka03 <146524718+pupetka03@users.noreply.github.com>
2023-12-20 14:57:08 +0100

    adminka

--- store/models.py
+++ store/models.py
@@ -4,3 +4,7 @@ class MobileCategory(models.Model):
     name = models.CharField(max_length=255, unique=True)
     order = models.PositiveIntegerField()
     is_visible = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f'{self.name}'
+
