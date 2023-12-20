fb7bf5d1d35f17eca52e260b36229081b6c5aafe
pupetka03 <146524718+pupetka03@users.noreply.github.com>
2023-12-20 14:44:21 +0100

    migrate

--- store/models.py
+++ store/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
 class MobileCategory(models.Model):
-    name = models.CharField(max_lenght=255, unique=True)
+    name = models.CharField(max_length=255, unique=True)
     order = models.PositiveIntegerField()
     is_visible = models.BooleanField(default=True)
