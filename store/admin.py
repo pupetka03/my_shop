b0de70ff1ddf1d271e022ae273c3eadbe5602b36
pupetka03 <146524718+pupetka03@users.noreply.github.com>
2023-12-20 22:34:43 +0100

    start

--- store/admin.py
+++ store/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import MobileCategory, Mobile
+from django.utils.safestring import mark_safe
 
 
 admin.site.register(MobileCategory)
@@ -7,12 +8,16 @@ admin.site.register(MobileCategory)
 @admin.register(Mobile)
 class MobileAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
-    list_display = ('id', 'category', 'name', 'price',  "is_visible")
-    
-    # slug
-    # description  
-    # photo
-    # order
+    list_display = ('id', 'category', 'name', 'price',  "is_visible", "photo_src_tag")
+    list_editable = ('category', 'name', 'price',  "is_visible")
+    search_fields = ('name',)
+    list_filter = ('category', "price", "is_visible",)
+
+    def photo_src_tag(self, obj):
+        if obj.photo:
+            return mark_safe(f"<img src='{obj.photo.url}' width=50>")
+
+    photo_src_tag.short_description = 'Mobile photo'
 
 
     
--- store/views.py
+++ store/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from .models import Mobile, MobileCategory
 # Create your views here.
+def main(request):
+    category = MobileCategory.objects.filter(is_visible=True)
+    return render()
