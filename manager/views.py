7702dcc02ecf43c559064eb75a05bcb1b5320e41
pupetka03 <146524718+pupetka03@users.noreply.github.com>
2024-01-09 00:03:16 +0100

    manager

--- manager/views.py
+++ manager/views.py
@@ -1,10 +1,15 @@
-from django.shortcuts import render
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import ListView
 from store.models import Mobile
 
-# Create your views here.
-class ManagerIndex(LoginRequiredMixin, ListView):
+
+
+class ManagerAccessMixin(UserPassesTestMixin):
+    def test_func(self):
+        return self.request.user.groups.filter(name='manager').exists()
+
+
+class ManagerIndex(LoginRequiredMixin, ManagerAccessMixin, ListView):
     template_name = 'manager_index.html'
     login_url = '/login/'
     model = Mobile
